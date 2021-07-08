9d97c6a7b6d61ff10168b6d6371ba4f545530dc7
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-07-08 21:12:57 +0530

    Configure Chakra-UI and Redux Toolit store in the App

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { ChakraProvider } from "@chakra-ui/react"
+import { store } from "./app/store";
+import { Provider } from "react-redux"
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store = { store } >
+      <ChakraProvider >
+        <App />
+      </ChakraProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
