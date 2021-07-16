77c5d66ac32ea021458ad6cbaaa1c25be437174c
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-07-16 22:01:26 +0530

    Refactor: Moved themeToggler and UserMenu as diferent Componennts

--- src/Pages/Home/Components/Navbar.tsx
+++ src/Pages/Home/Components/Navbar.tsx
@@ -1,15 +1,33 @@
-import { Flex ,Text, useColorMode, HStack, IconButton, useDisclosure} from "@chakra-ui/react";
-import { FaSun , FaMoon } from 'react-icons/fa';
-import { UserLoginDrawer } from "./UserLoginDrawer"
-import { useAppDispatch, useAppSelector } from "../../../app/Hooks/hooks"
-import { useEffect } from "react";
-
+import { useEffect } from "react"
+import { 
+    Button,
+    Flex,
+    HStack, 
+    Text, 
+    useDisclosure 
+} from "@chakra-ui/react";
+import { UserLoginDrawer , UserMenu , DarkModeToggler } from "../Components"
+import { useAppSelector, useAppDispatch } from "../../../app/Hooks/hooks"
+import { getUserData } from "../../../app/Features/UserProfile/UsersSlice"
+import { logOutUser } from "../../../app/Features/Auth/AuthSlice"
 
 export const Navbar = () => {
-    const { colorMode ,toggleColorMode } = useColorMode()
     const { isOpen , onOpen , onClose } = useDisclosure()
 
     const { token } = useAppSelector((state) => state.auth)
+    const { status } = useAppSelector((state)=> state.user)
+    const userDispatch = useAppDispatch()
+    const authDispatch = useAppDispatch()
+    useEffect(() => {
+        userDispatch( getUserData(token) )
+    }, [ userDispatch , token]);
+    
+    useEffect(()=>{
+        if(status === "error"){
+            authDispatch(logOutUser());
+        }
+    },[status , authDispatch])
+    console.log({status})
     return (
         <Flex 
             w="100%" 
@@ -19,14 +37,11 @@ export const Navbar = () => {
         >
             <Text>Brand</Text>
             <HStack>
-                <IconButton 
-                    aria-label="Toggle Theme" 
-                    icon = { colorMode === "light"? <FaMoon /> : <FaSun/> } 
-                    borderRadius={8}
-                    onClick = { ()=>toggleColorMode() } 
-                />
-                { !token && <Text onClick={onOpen} cursor="pointer">Login</Text> }
-                <UserLoginDrawer isDrawerOpen={isOpen} onDrawerClose={onClose} />
+                <DarkModeToggler />
+                { token && <UserMenu /> }
+                {/* Login button gets automatically clicked when the logout is option is chosen */}
+                { !token && <Button onClick={onOpen} cursor="pointer">Login</Button> }
+                { !token && <UserLoginDrawer isDrawerOpen={isOpen} onDrawerClose={onClose} />}
             </HStack>
         </Flex>
     );
