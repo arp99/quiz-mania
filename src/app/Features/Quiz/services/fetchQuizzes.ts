661202badcba3434b39940fb8db08689342770a0
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-07-17 16:47:30 +0530

    Update token type

--- src/app/Features/Quiz/services/fetchQuizzes.ts
+++ src/app/Features/Quiz/services/fetchQuizzes.ts
@@ -1,13 +1,10 @@
-import axios, { AxiosResponse } from "axios"
-import { AllQuizes , Quiz} from "../../../Types/Quiz.types"
-
-/**TODO: Narrow down type checking on the axios response which was not working as expected...Need Help! */
+import axios from "axios"
 
 export const fetchAllQuizzes = () => {
     return axios.get('https://quiz-mania-backend.arp99.repl.co/api/quizes')
 }
 
-export const fetchQuizById = ( quizId : string , token : string ) => {
+export const fetchQuizById = ( quizId : string , token : string | null ) => {
     
     return axios.get(`https://quiz-mania-backend.arp99.repl.co/api/quizes/${quizId}`,{
         headers : {
