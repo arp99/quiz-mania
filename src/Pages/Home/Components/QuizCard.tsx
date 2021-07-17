32ff11491c297d5605dddbb5dabaea5f87fc1ff5
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-07-17 17:05:35 +0530

    Load questions on play

--- src/Pages/Home/Components/QuizCard.tsx
+++ src/Pages/Home/Components/QuizCard.tsx
@@ -1,15 +1,48 @@
 import { Box , Image, Heading, Text, Button ,VStack} from "@chakra-ui/react";
+import { useAppDispatch, useAppSelector } from "../../../app/Hooks/hooks";
+import { loadQuizById } from "../../../app/Features/Quiz/QuizSlice"
+import { useToast } from "@chakra-ui/react"
+import { useNavigate } from "react-router-dom";
 
 export const QuizCard = ({ 
         QuizName , 
         description , 
-        imageUrl
+        imageUrl,
+        quizId
     } 
     : { 
         QuizName : string , 
         description : string , 
-        imageUrl : string 
+        imageUrl : string,
+        quizId : string 
     }) => {
+    const { authStatus, token } = useAppSelector((state) =>state.auth)
+    const toast = useToast()
+    const { currentQuiz, currQuizLoadStatus } = useAppSelector((state)=>state.quiz)
+    const quizDispatch = useAppDispatch()
+    const navigate = useNavigate()
+
+    const QuizPlayButtonClicked = async() => {
+        
+        if( authStatus !== "fulfilled"){
+            return toast({
+                title: "Login First to Enter Quiz",
+                status: "warning",
+                duration: 2000,
+                isClosable: true,
+                position:"top"
+              })
+            }
+            else{
+                if( !currentQuiz ){
+                    await quizDispatch( loadQuizById( { quizId , token } ))
+                    navigate(`/quiz/${quizId}`)
+                }else{
+                    navigate(`/quiz/${quizId}`)
+                }
+        }
+    }
+    
     return (
         <Box maxW="sm" borderRadius="lg" overflow="hidden" borderWidth="2px">
             <Image src={imageUrl} alt="Quiz Image" aria-label="Quiz image" w="100%" h="15rem" />
@@ -18,7 +51,16 @@ export const QuizCard = ({
                     <Heading>{QuizName}</Heading>
                     <Text>{description}</Text>
                 </Box>
-                <Button colorScheme="teal" px="2rem" size="lg">Play</Button>
+                <Button 
+                    colorScheme="teal" 
+                    isLoading={ currQuizLoadStatus === "loading" }
+                    loadingText={ "Loading Questions" }
+                    px="2rem" 
+                    size="lg" 
+                    onClick={ ()=> QuizPlayButtonClicked() }
+                >
+                    Play
+                </Button>
             </VStack>
         </Box>
     );
