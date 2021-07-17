387182665e8667a94f6291e8294062a5be29db42
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-07-18 02:54:07 +0530

    Add functionality to start quiz button
    Click start quiz to close instructions and start quiz

--- src/Pages/QuizPlay/QuizPlay.tsx
+++ src/Pages/QuizPlay/QuizPlay.tsx
@@ -5,8 +5,10 @@ import { loadQuizById } from "../../app/Features/Quiz/QuizSlice"
 import { Spinner } from "../../Components";
 import { InstructionContainer } from "./Components/InstructionContainer";
 import { VStack } from "@chakra-ui/react";
+import { QuestionContainer } from "./Components/QuestionContainer";
 
 export const QuizPlay = () => {
+    const [ startQuiz , setStartQuiz ] = useState<boolean>(false);
     const { currentQuiz } = useAppSelector((state)=>state.quiz)
     const { token } = useAppSelector((state) => state.auth)
     const quizDispatch = useAppDispatch()
@@ -20,7 +22,8 @@ export const QuizPlay = () => {
     return (
         <VStack w={'100%'}>
             { !currentQuiz && <Spinner />}
-            { currentQuiz && <InstructionContainer currentQuiz={currentQuiz} /> }
+            { currentQuiz && !startQuiz && <InstructionContainer currentQuiz={currentQuiz} setStartQuiz={setStartQuiz} /> }
+            { currentQuiz && startQuiz &&  <QuestionContainer currentQuiz={currentQuiz} />}
         </VStack>
     );
 }
