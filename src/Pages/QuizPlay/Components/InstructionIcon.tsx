f7f228ebab0af1eb8821fe482793585dd59542a4
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-07-17 16:55:27 +0530

    Add custom instruction icon component

---
+++ src/Pages/QuizPlay/Components/InstructionIcon.tsx
@@ -0,0 +1,7 @@
+import instuctionIcon from "../assets/instruction.png"
+
+export const InstructionIcon = (props : any) => {
+    return (
+        <img src={instuctionIcon} alt="instruction icon" style={{...props}} />
+    );
+}
