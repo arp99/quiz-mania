1a0af607383d866706ceb1dec59c5386d716814e
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-07-17 17:01:48 +0530

    Add quizplay in the private route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { Home } from "./Pages"
+import { Home, QuizPlay } from "./Pages"
 import { Routes, Route } from "react-router-dom"
+import { PrivateRoute } from "./PrivateRoute/PrivateRoute"
+
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <PrivateRoute path="/quiz/:quizId" element={<QuizPlay />} />
       </Routes>
     </div>
   );
