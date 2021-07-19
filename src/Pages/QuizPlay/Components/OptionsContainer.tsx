910541d22629cb4fe9398238db0626753058a227
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-07-20 00:31:39 +0530

    Feat: Check answer, update score  on click and move to next question

--- src/Pages/QuizPlay/Components/OptionsContainer.tsx
+++ src/Pages/QuizPlay/Components/OptionsContainer.tsx
@@ -1,12 +1,75 @@
 import { Box, useColorModeValue } from "@chakra-ui/react"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { 
+    disableOptionClick, 
+    enableOptionClick, 
+    updateScore, 
+    updateQuestionNumber, 
+} from "../../../app/Features/Quiz/QuizSlice"
+import { useAppDispatch, useAppSelector } from "../../../app/Hooks/hooks"
+import { Question } from "../../../app/Types/Quiz.types"
 
 export const OptionsContainer = ( 
-    { _id , option, isRight } 
+    { _id , option, isRight , currentQuestion } 
     :
-    { _id: string; option: string; isRight: boolean }) => {
+    { _id: string; option: string; isRight: boolean, currentQuestion: Question }) => {
 
+        const [ selectedId , setSelectedId ] = useState<string>('')
+        const { optionClickDisabled, currentQuiz, currQuestionNumber } = useAppSelector(( state ) => state.quiz )
+        const quizDispatch = useAppDispatch()
+        const navigate = useNavigate()
+
+        const rightAnswer = useColorModeValue('green.200','green.500')
+        const wrongAnswer = useColorModeValue('red.200', 'red.500')
+        const defaultColor = useColorModeValue('gray.200', 'gray.800')
+
+        const showNextQuestion = () => {
+            currQuestionNumber + 1 === currentQuiz?.questions.length ? navigate("/") : quizDispatch( updateQuestionNumber() )
+        }
+
+        const checkAnswerAndShowNextQuestion = () =>{
+            if( !isRight ){
+                const { negativePoints } = currentQuestion
+                quizDispatch( updateScore( negativePoints ? -1 * negativePoints : 0 ))
+            }
+            else if( isRight ){
+                const { points } = currentQuestion
+                quizDispatch( updateScore(points) )
+            }
+            showNextQuestion()
+        }
+
+        const getOptionStyle = () =>{
+            if( optionClickDisabled ){
+                if(isRight){
+                    return rightAnswer;
+                }
+                else if( _id === selectedId ){
+                    return wrongAnswer;
+                }
+            }
+            return defaultColor;    
+        }
+        const optionChosenHandler = () => {
+            quizDispatch( disableOptionClick() )
+            setSelectedId(_id)
+            setTimeout(()=>{
+                checkAnswerAndShowNextQuestion()
+                quizDispatch( enableOptionClick() )
+            },1000)
+        }
     return (
-       <Box as="button" bg={ useColorModeValue('gray.200', 'gray.800') } h={'max-content'} rounded={'sm'} p={4} textAlign="left">
+       <Box 
+            as="button" 
+            disabled={ optionClickDisabled }
+            onClick={ ()=>optionChosenHandler() }
+            bg={ getOptionStyle() } 
+            h={'max-content'} 
+            rounded={'sm'} 
+            p={4} 
+            textAlign="left"
+        >
            { option }
        </Box> 
     );
