d42484c480eff2e7b1ddfc0386544860ce2d9220
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-07-23 02:27:51 +0530

    Add routes for result page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Home, QuizPlay } from "./Pages"
 import { Routes, Route } from "react-router-dom"
 import { PrivateRoute } from "./PrivateRoute/PrivateRoute"
+import { Results } from "./Pages/Results/Results"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <PrivateRoute path="/quiz/:quizId" element={<QuizPlay />} />
+        <PrivateRoute path="/results" element={<Results />} />
       </Routes>
     </div>
   );
