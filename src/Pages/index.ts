0aea05459854e89b882d88b40fc9c05210bd747d
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-07-27 21:15:44 +0530

    Export results and userprofile page

--- src/Pages/index.ts
+++ src/Pages/index.ts
@@ -1,2 +1,4 @@
 export { Home } from "./Home/Home"
 export { QuizPlay } from "./QuizPlay/QuizPlay"
+export { Results } from "./Results/Results"
+export { Userprofile } from "./Userprofile/Userprofile"
