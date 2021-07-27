a3f00a4bc6f9199d31f28841bc3084c0c5f21355
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-07-27 21:16:51 +0530

    Add route for user profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import { Home, QuizPlay } from "./Pages"
+import { Home, QuizPlay, Results, Userprofile } from "./Pages"
 import { Routes, Route } from "react-router-dom"
 import { PrivateRoute } from "./PrivateRoute/PrivateRoute"
-import { Results } from "./Pages/Results/Results"
 
 function App() {
   return (
@@ -10,6 +9,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <PrivateRoute path="/quiz/:quizId" element={<QuizPlay />} />
         <PrivateRoute path="/results" element={<Results />} />
+        <PrivateRoute path="/user-profile" element={<Userprofile />} />
       </Routes>
     </div>
   );
