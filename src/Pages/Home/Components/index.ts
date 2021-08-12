8198feadf59de96f308b1215de7be34fb129e964
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-08-12 18:04:17 +0530

    Remove register and login form exports

--- src/Pages/Home/Components/index.ts
+++ src/Pages/Home/Components/index.ts
@@ -1,7 +1,4 @@
 export { DarkModeToggler } from "./DarkModeToggler";
-export { LoginForm } from "./LoginForm";
 export { Navbar } from "./Navbar";
 export { QuizCard } from "./QuizCard";
-export { RegisterForm } from "./RegisterForm";
-export { UserLoginDrawer } from "./UserLoginDrawer";
 export { UserMenu } from "./UserMenu"
