e42243374907c4fcd0ce69f25a0eb546f55c9317
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-08-12 20:41:23 +0530

    Style: Updated layout for login page

--- src/Pages/LoginSignUp/Login/LoginForm.tsx
+++ src/Pages/LoginSignUp/Login/LoginForm.tsx
@@ -1,7 +1,9 @@
 import {     
     Box,
     Button,
+    Grid,
     Heading,
+    Image,
     Stack,
     Text,
     VStack
@@ -11,28 +13,16 @@ import { AiOutlineLogin } from "react-icons/ai"
 import { useAppDispatch, useAppSelector } from "../../../app/Hooks/hooks"
 import { loginUser } from "../../../app/Features/Auth/AuthSlice"
 import { Form, Formik } from "formik";
-import * as Yup from "yup"
 import { ChakraFormController } from "../Components/ChakraFormController";
 import { useNavigate } from "react-router-dom";
+import LoginImage from "../assets/quiz-vector.jpg"
+import { FormValues } from "./FormConfig/FieldType";
+import { initialValues, validationSchema } from "./FormConfig";
 
 export const LoginForm = () => {
     const { authStatus } = useAppSelector((state) => state.auth )
     const authDispatch = useAppDispatch()
 
-    type FormValues = {
-        emailId: string;
-        password: string;
-    }
-
-    const initialValues : FormValues = {
-        emailId : "",
-        password : ""
-    }
-
-    const validationSchema = Yup.object({
-        emailId: Yup.string().email("Invalid Email Format").required("Required"),
-        password: Yup.string().required("Required")
-    }) 
     const loginHandler = (values : FormValues , onSubmitProps : any)  =>{
         const { emailId, password } = values
         const { resetForm, isSubmitting } = onSubmitProps
@@ -44,59 +34,65 @@ export const LoginForm = () => {
     const navigate = useNavigate()
 
     return (
-        <Stack spacing="2rem" h="100%" justifyContent="center">
-            <Heading fontSize="3xl">Login</Heading>
-            <Formik 
-                initialValues={initialValues}
-                validationSchema={validationSchema}
-                onSubmit={loginHandler}
-            >
-                <Form >
-                    <VStack spacing="1.5rem" alignItems="flex-start">
-                        <ChakraFormController
-                            name="emailId"
-                            id="emailId"
-                            label="Email Address"
-                            placeholder="Example: any@gmail.com" 
-                            type="email"
-                            control="chakraFormInput"
-                            Required
-                        />
-                        <ChakraFormController 
-                            name="password"
-                            id="password"
-                            label="Password"
-                            placeholder="Enter Your Password"
-                            type="password"
-                            control="chakraFormInput"
-                            Required
-                        />
-                        <Button 
-                            rightIcon = {<AiOutlineLogin />}
-                            type="submit"
-                            isLoading = { authStatus === "loading" }
-                            loadingText = "Logging in"
-                        >
-                            Login
-                        </Button> 
-                    </VStack>
-                </Form>
-            </Formik>
-
+        <Grid h="100vh" templateColumns="repeat(2,1fr)" gap="24">
             <Box>
-                <Text fontSize="sm" letterSpacing="0.1rem">
-                    Not registered Yet? 
-                    <Text 
-                        as="span" 
-                        decoration="underline" 
-                        color="red.300" 
-                        cursor="pointer"
-                        onClick = { ()=> navigate("/register") }
-                    >
-                        Register
-                    </Text>
-                </Text>
+                <Image src={LoginImage} h="100%" w="100%" />
             </Box>
-        </Stack>
+            <Stack spacing="2rem" h="100%" w="80%" justifyContent="center">
+                <Heading fontSize="3xl">Login</Heading>
+                <Formik 
+                    initialValues={initialValues}
+                    validationSchema={validationSchema}
+                    onSubmit={loginHandler}
+                >
+                    <Form>
+                        <VStack spacing="1.5rem" alignItems="flex-start">
+                            <ChakraFormController
+                                name="emailId"
+                                id="emailId"
+                                label="Email Address"
+                                placeholder="Example: any@gmail.com" 
+                                type="email"
+                                control="chakraFormInput"
+                                Required
+                            />
+                            <ChakraFormController 
+                                name="password"
+                                id="password"
+                                label="Password"
+                                placeholder="Enter Your Password"
+                                type="password"
+                                control="chakraFormInput"
+                                Required
+                            />
+                            <Button 
+                                rightIcon = {<AiOutlineLogin />}
+                                type="submit"
+                                isLoading = { authStatus === "loading" }
+                                loadingText = "Logging in"
+                            >
+                                Login
+                            </Button> 
+                        </VStack>
+                    </Form>
+                </Formik>
+
+                <Box>
+                    <Text fontSize="sm" letterSpacing="0.1rem">
+                        Not registered Yet? 
+                        <Text 
+                            as="span" 
+                            decoration="underline" 
+                            color="red.300" 
+                            cursor="pointer"
+                            onClick = { ()=> navigate("/register") }
+                        >
+                            Register
+                        </Text>
+                    </Text>
+                </Box>
+            </Stack>
+        </Grid>
+        
     );
 }
