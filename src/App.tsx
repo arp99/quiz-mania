192afddc969c69bc4229528e1eb0ac01cd0c160d
Arpan Mondal <arpan19991025@gmail.com>
2021-11-28 17:43:43 +0530

    Add navbar to whole app, Add route for profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import { Home, LoginForm, QuizPlay, RegisterForm } from "./Pages"
+import { Home, LoginForm, Profile, QuizPlay, RegisterForm } from "./Pages"
 import { Routes, Route } from "react-router-dom"
 import { PrivateRoute } from "./PrivateRoute/PrivateRoute"
 import { Results } from "./Pages/Results/Results"
 import "./App.css"
+import { Navbar } from "./Components"
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/register" element={<RegisterForm />} />
         <PrivateRoute path="/quiz/:quizId" element={<QuizPlay />} />
+        <PrivateRoute path="/profile" element={<Profile />} />
         <PrivateRoute path="/results" element={<Results />} />
       </Routes>
     </div>
