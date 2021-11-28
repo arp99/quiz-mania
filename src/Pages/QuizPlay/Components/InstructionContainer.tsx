42307a0d3049b4ad4817f022a07e90ea164115c2
Arpan Mondal <arpan19991025@gmail.com>
2021-11-28 17:48:38 +0530

    Styles:update layout of instructions with leaderboard

--- src/Pages/QuizPlay/Components/InstructionContainer.tsx
+++ src/Pages/QuizPlay/Components/InstructionContainer.tsx
@@ -1,77 +1,90 @@
-import { 
-    Button, 
-    Center, 
-    Heading,
-    HStack,
-    Text, 
-    VStack,
-    useColorModeValue 
-} from "@chakra-ui/react"
+import {
+  Button,
+  Center,
+  Heading,
+  Text,
+  useColorModeValue,
+  Flex,
+  Box,
+  UnorderedList,
+  ListItem,
+} from "@chakra-ui/react";
 import { Quiz } from "../../../app/Types/Quiz.types";
-import { InstructionIcon } from "./InstructionIcon"
-import { BsInfo } from "react-icons/bs";
-import { FcAlarmClock } from "react-icons/fc";
+import { Leaderboard } from "./Leaderboard";
 
-export const InstructionContainer = (
-    
-    { currentQuiz, setStartQuiz } 
-    : 
-    { 
-        currentQuiz : Quiz | null,
-        setStartQuiz : React.Dispatch<React.SetStateAction<boolean>> 
-    } ) => {
-    
-    const { name, questions } = { ...currentQuiz }    
-    return (
-        <VStack justify="center" h={'100vh'}  p={0} >
-            <VStack boxShadow={'2xl'}  maxW="40vw" minH={'50vh'}  rounded={'xl'} p={8} bg={useColorModeValue('white', 'gray.700')}  >
-                <Center >
-                    <InstructionIcon width="5rem" />
-                </Center>
-                <Heading>{ name }</Heading>
-                <HStack alignSelf="flex-start" >
-                    <BsInfo size="2rem" />
-                    <Text fontSize="lg" letterSpacing="0.15rem" >There are total { questions?.length } questions</Text>
-                </HStack>
-                <HStack alignSelf="flex-start">
-                    <BsInfo size="2rem" />
-                    <Text fontSize="lg" letterSpacing="0.15rem" >
-                        10 marks for every{' '} 
-                        <Text as="span" fontWeight="bold" color="green.400" letterSpacing="0.15rem">
-                            Right Answer{' '}
-                        </Text>
-                        and 5 negative marks for{' '} 
-                        <Text as="span" fontWeight="bold" color="red.400" letterSpacing="0.15rem">
-                            Wrong Answer
-                        </Text>
-                    </Text>
-                </HStack>
-                <HStack alignSelf="flex-start" >
-                    <BsInfo size="2rem" />
-                    <Text fontSize="lg" letterSpacing="0.15rem" >
-                        There will be a Timer for Whole Quiz
-                    </Text>
-                    <FcAlarmClock size="2rem" />
-                </HStack>
-                <HStack alignSelf="flex-start" >
-                    <BsInfo size="2rem" />
-                    <Text fontSize="lg" letterSpacing="0.15rem" >
-                        You cannot Skip any Question 😅
-                    </Text>
-                </HStack>
-                <Center>
-                    <Button 
-                        bg={'blue.500'} 
-                        rounded={'lg'} 
-                        color={'white'} 
-                        flex={'1 0 auto'} 
-                        letterSpacing="0.15rem"
-                        onClick={()=>setStartQuiz(true)}
-                    >
-                    Start Quiz
-                    </Button>
-                </Center>
-            </VStack>
-        </VStack>
-    );
-}
+export const InstructionContainer = ({
+  currentQuiz,
+  setStartQuiz,
+}: {
+  currentQuiz: Quiz | null;
+  setStartQuiz: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  const { name, questions } = { ...currentQuiz };
+  return (
+    <Box w="full" h="100%" p={0} pb={{ base:"20", md:"0"}}>
+      <Box
+        boxShadow={"2xl"}
+        w={{ base: "90%", md: "85%", lg:"60%" }}
+        minH="60vh"
+        rounded={"xl"}
+        p={8}
+        mx="auto"
+        mt="20"
+        bg={useColorModeValue("white", "gray.700")}
+      >
+        <Heading>{name}</Heading>
+        <Flex minH="60vh" w="100%" direction={{ base:"column", md:"row"}}>
+          <Box h="inherit" w={{ base:"100%", md:"50%" }} border="2px" borderColor="gray.500" mb="2" p="2">
+            <Text mb="2" fontSize="lg" letterSpacing="0.15rem">
+              Total Questions: {questions?.length}
+            </Text>
+            <Text mb="2" fontSize="lg" letterSpacing="0.15rem">
+              Duration: 5 minutes
+            </Text>
+            <Text mb="2" fontSize="lg" letterSpacing="0.15rem">
+              Rules:
+            </Text>
+
+            <UnorderedList pl="4">
+              <ListItem>
+                <Text mb="2" fontSize="lg" letterSpacing="0.15rem">
+                  Submit Quiz before the time
+                </Text>
+              </ListItem>
+              <ListItem>
+                <Text mb="2" fontSize="lg" letterSpacing="0.15rem">
+                  4 marks for Right Answer
+                </Text>
+              </ListItem>
+              <ListItem>
+                <Text mb="2" fontSize="lg" letterSpacing="0.15rem">
+                  -1 for every wrong answer
+                </Text>
+              </ListItem>
+              <ListItem>
+                <Text mb="2" fontSize="lg" letterSpacing="0.15rem">
+                  Good luck see you on leaderboard
+                </Text>
+              </ListItem>
+            </UnorderedList>
+            <Center>
+              <Button
+                bg={"blue.500"}
+                rounded={"lg"}
+                color={"white"}
+                letterSpacing="0.15rem"
+                w="max"
+                onClick={() => setStartQuiz(true)}
+              >
+                Start Quiz
+              </Button>
+            </Center>
+          </Box>
+          <Box w={{ base:"100%", md:"50%" }}>
+              <Leaderboard />
+          </Box>
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
